e38a1efa39fee25e6b1081a09bc19a90c2c301f3
Marius Bronner <marius.bronner@icloud.com>
2019-11-01 20:23:54 +0100

    added planetarium

---
+++ Planetarium/Planetarium/GameViewController.swift
@@ -0,0 +1,136 @@
+//
+//  GameViewController.swift
+//  Planetarium
+//
+//  Created by Marius Bronner on 01.11.19.
+//  Copyright © 2019 Marius Bronner. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+import SceneKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // create a new scene
+        let scene = SCNScene()
+        
+        // create and add a camera to the scene
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        scene.rootNode.addChildNode(cameraNode)
+        
+        // place the camera
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
+        
+        // create and add a light to the scene
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light!.type = .omni
+        lightNode.light!.color = UIColor(red: 1, green: 0.95, blue: 0.8, alpha: 1)
+        lightNode.light!.intensity = 2000
+        
+        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
+        scene.rootNode.addChildNode(lightNode)
+        
+        // create and add an ambient light to the scene
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = SCNLight()
+        ambientLightNode.light!.type = .ambient
+        ambientLightNode.light!.color = UIColor.darkGray
+        ambientLightNode.light!.intensity = 250
+        scene.rootNode.addChildNode(ambientLightNode)
+        
+        // retrieve the SCNView
+        let scnView = self.view as! SCNView
+        
+        // set the scene to the view
+        scnView.scene = scene
+        
+        // allows the user to manipulate the camera
+        scnView.allowsCameraControl = true
+        
+        // show statistics such as fps and timing information
+        scnView.showsStatistics = true
+        
+        // configure the view
+        scnView.backgroundColor = UIColor.black
+        
+        // add a tap gesture recognizer
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        scnView.addGestureRecognizer(tapGesture)
+        
+        // add a sphere
+//        let sphere = SCNSphere(radius: 10)
+//        let sphere_node = SCNNode(geometry: sphere)
+//        sphere_node.light = SCNLight()
+//        sphere_node.light!.type = .omni
+//        sphere_node.light!.color = UIColor(red: 1, green: 0.95, blue: 0.8, alpha: 1)
+//        sphere_node.light!.intensity = 2000
+//        sphere_node.position = SCNVector3(0, 50, 50)
+//        scene.rootNode.addChildNode(sphere_node)
+        
+        let sphere2 = SCNSphere(radius: 2)
+        let texture = UIImage(named: "earth_texture.png")
+        sphere2.firstMaterial?.diffuse.contents = texture
+        let sphere2_node = SCNNode(geometry: sphere2)
+        scene.rootNode.addChildNode(sphere2_node)
+        
+    }
+    
+    @objc
+    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
+        // retrieve the SCNView
+        let scnView = self.view as! SCNView
+        
+        // check what nodes are tapped
+        let p = gestureRecognize.location(in: scnView)
+        let hitResults = scnView.hitTest(p, options: [:])
+        // check that we clicked on at least one object
+        if hitResults.count > 0 {
+            // retrieved the first clicked object
+            let result = hitResults[0]
+            
+            // get its material
+            let material = result.node.geometry!.firstMaterial!
+            
+            // highlight it
+            SCNTransaction.begin()
+            SCNTransaction.animationDuration = 0.5
+            
+            // on completion - unhighlight
+            SCNTransaction.completionBlock = {
+                SCNTransaction.begin()
+                SCNTransaction.animationDuration = 0.5
+                
+                material.emission.contents = UIColor.black
+                
+                SCNTransaction.commit()
+            }
+            
+            material.emission.contents = UIColor.red
+            
+            SCNTransaction.commit()
+        }
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+}
